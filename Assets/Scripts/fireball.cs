471ddb1373b839c18d0bfa6803366c7f99c70037
longestcow <83398131+longestcow@users.noreply.github.com>
2023-06-28 09:41:54 -0600

    comments fireball

--- Assets/Scripts/fireball.cs
+++ Assets/Scripts/fireball.cs
@@ -8,14 +8,14 @@ public class fireball : MonoBehaviour
     int a = 1;
     void Start()
     {
-        if(transform.rotation.z==-180){
+        if(transform.rotation.z==-180){ // if fireball is rotated, move backwards
             a=-1;
             print("a");
         }
     }
     void Update() {
         if(a==1)
-            transform.position += new Vector3(0.1f,0,0);
+            transform.position += new Vector3(0.1f,0,0); // this doesn't work and i dont know why. i tried several ways and stopped trying after this one since i was out of time
         else
             transform.position -= new Vector3(0.1f,0,0);
 
@@ -25,7 +25,7 @@ public class fireball : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.name.StartsWith("death") || other.name.StartsWith("Floor"))
+        if(other.name.StartsWith("death") || other.name.StartsWith("Floor")) // kill object when it gets too far away or collides with a platform
             Destroy(gameObject);
     }
 
